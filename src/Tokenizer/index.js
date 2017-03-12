4f7c8465899e8dbe926b892a6225bb0778355100
Taylor Growden <taylorgrowden@gmail.com>
2017-03-12 12:05:33 -0400

    refine regex when rebuilding a tokenized array

--- src/Tokenizer/index.js
+++ src/Tokenizer/index.js
@@ -6,6 +6,31 @@ const _tokenizer = new natural.TreebankWordTokenizer()
 class Tokenizer {
 
     /**
+     * Regular expressions dictionary
+     * 
+     * @readonly
+     * @static
+     * 
+     * @memberOf Tokenizer
+     */
+    static get regexp() {
+        return {
+            contractions: {
+                match: /\s('ll|'re|'ve|n't|'s|'m|'d)\b/ig,
+                replace: '$1'
+            },
+            punctuation: {
+                match: /\s([.!?\',:%])/g,
+                replace: '$1'
+            },
+            money: {
+                match: /(\$)\s/g,
+                replace: '$1'
+            }
+        }
+    }
+
+    /**
      * Splits a string into an array of individual tokens
      * 
      * @static
@@ -32,8 +57,10 @@ class Tokenizer {
             throw new Error('Tokenizer can only rebuild an array.')
         }
         let text = tokenizedArray.join(' ')
-        text = text.replace(/\s([.!?\',:%])/g, '$1')
-        text = text.replace(/(\$)\s(\w)/g, '$1$2')
+        for (let regexp in Tokenizer.regexp) {
+            text = text.replace(Tokenizer.regexp[regexp].match, Tokenizer.regexp[regexp].replace)
+        }
+
         return text
     }
 
