aeb347a9bfe91d95f199b7efadc265243b500392
Taylor Growden <taylorgrowden@gmail.com>
2017-03-11 19:25:38 -0500

    add strict mode to _getRandomWord()

--- src/MarkovChain/index.js
+++ src/MarkovChain/index.js
@@ -64,13 +64,23 @@ class MarkovChain {
     /**
      * Gets a random word
      * 
+     * @param {any} strict If === false, return the first token. Otherwise, return an uppercase word
      * @returns {String} A random word from the word list
      * 
      * @memberOf MarkovChain
      */
-    _getRandomWord() {
+    _getRandomWord(strict) {
         const list = this.wordList
-        return list[Math.floor(list.length * Math.random())]
+        let word = list[Math.floor(list.length * Math.random())]
+
+        if (strict !== false) {
+            let pattern = /[A-Z]/
+            while (!word.charAt(0).match(pattern)) {
+                word = list[Math.floor(list.length * Math.random())]
+            }
+        }
+
+        return word
     }
 
     /**
@@ -104,7 +114,7 @@ class MarkovChain {
     /**
      * Method for generating a Markov chain
      * 
-     * @param {String|null} firstWord The word to start the Markov chain
+     * @param {String|Boolean} firstWord The word to start the Markov chain; if false, strict mode is overriden
      * @param {Number} limit The maximum number of words for the Markov chain
      * @returns {String}
      * 
@@ -114,8 +124,9 @@ class MarkovChain {
         limit = limit || this.limit
         const chain = []
 
+        let strict = firstWord
         while (!firstWord) {
-            firstWord = this._getRandomWord()
+            firstWord = this._getRandomWord(strict)
         }
         chain.push(firstWord)
         let nextWord = this._getNextWord(firstWord)
